4c13e20da59ed10e6487f6754e9b3b6236f0324d
ahmadsaputra19 <rebahankan22@gmail.com>
2024-10-22 11:42:15 +0700

    menambahkan sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const cacheName = "aamedia";
-const preCache = ["./", "./style.css", "./script.js", "./ScheherazadeNew-Medium.ttf", "./duror.html"];
+const preCache = ["./", "./style.css", "./script.js", "./ScheherazadeNew-Medium.ttf"];
 
 self.addEventListener("install", (e) => {
   console.log("Service worker installed");
